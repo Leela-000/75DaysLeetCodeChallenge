c01613cffe9422cc1d0622a9eb3ea40c2b453312
Leela Borra <leelaborra0@gmail.com>
2026-05-08 23:01:57 +0530

    Time: 7 ms (67.65%), Space: 43.9 MB (74.94%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+
+        // Count characters in s1
+        for (char c : s1.toCharArray()) {
+            s1Count[c - 'a']++;
+        }
+
+        // Sliding window over s2
+        for (int i = 0; i < s2.length(); i++) {
+            s2Count[s2.charAt(i) - 'a']++;
+
+            // Maintain window size equal to s1 length
+            if (i >= s1.length()) {
+                s2Count[s2.charAt(i - s1.length()) - 'a']--;
+            }
+
+            // Compare frequency arrays
+            if (matches(s1Count, s2Count)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean matches(int[] s1Count, int[] s2Count) {
+        for (int i = 0; i < 26; i++) {
+            if (s1Count[i] != s2Count[i]) return false;
+        }
+        return true;
+    }
+}
