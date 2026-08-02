8d8014b534136e8892067cd5bcd8b9bfd33a42de
Leela Borra <leelaborra0@gmail.com>
2026-08-02 13:31:57 +0530

    Time: 6 ms (39.28%), Space: 47 MB (33.69%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -3,13 +3,13 @@ class Solution {
         int n=nums.length;
         int max=Arrays.stream(nums).max().orElse(Integer.MIN_VALUE);
         int min=Arrays.stream(nums).min().orElse(Integer.MAX_VALUE);
+        boolean[] map=new boolean[101];
         List<Integer> res=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
         for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+            map[num]=true;
         }
-        for(int i=min;i<=max;i++){
-            if(!map.containsKey(i)){
+        for(int i=min;i<max;i++){
+            if(!map[i]){
                 res.add(i);
             }
         }
